2c548c2aeae780a1f148330868fcbf0d22cc2555
Lesosa12 <Lesosa@s.sfusd.edu>
2024-02-26 20:39:00 -0800

    progress check 8 done

--- Heart Platformer/world.gd
+++ Heart Platformer/world.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@export var next_level: PackedScene
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +11,13 @@ func _ready():
 func show_level_completed():
 	level_completed.show()
 	get_tree().paused = true
+	if not next_level is PackedScene: return
+	await LevelTransistion.fade_to_black()
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(next_level)
+	LevelTransistion.fade_from_black()
+	
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
