0670a18770fbc0534aa12f521b1c045534fed621
Lesosa12 <Lesosa@s.sfusd.edu>
2024-03-03 14:06:50 -0800

    progress check 14

--- Heart Platformer/world.gd
+++ Heart Platformer/world.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
 @export var next_level: PackedScene
+
+var level_time = 0.0
+var start_level_msec = 0.0
+
 @onready var level_completed = $CanvasLayer/LevelCompleted
 @onready var start_in = %StartIn
 @onready var start_in_label = %StartInLabel
 @onready var animation_player = $AnimationPlayer
+@onready var level_time_label = %LevelTimeLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,20 +21,37 @@ func _ready():
 	await animation_player.animation_finished
 	get_tree().paused = false
 	start_in.visible = false
+	start_level_msec = Time.get_ticks_msec()
 
+func _process(delta):
+	level_time = Time.get_ticks_msec() - start_level_msec
+	level_time_label.text = str(level_time / 1000.0)	
 
-func show_level_completed():
-	level_completed.show()
-	get_tree().paused = true
-	await get_tree().create_timer(1.0).timeout
+func retry():
+	await LevelTransistion.fade_from_black()
+	get_tree().paused = false
+	get_tree().change_scene_to_file(scene_file_path)
+
+func go_to_next_level():
 	if not next_level is PackedScene: return
 	await LevelTransistion.fade_to_black()
 	get_tree().paused = false
 	get_tree().change_scene_to_packed(next_level)
 
+func show_level_completed():
+	level_completed.show()
+	level_completed.retry_button.grab_focus()
+	get_tree().paused = true
+	
 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+
+
+func _on_level_completed_next_level():
+	retry()
+
+
+func _on_level_completed_retry():
+	go_to_next_level()
